13267d2372963dbd8fc76170da1dca236a52200f
Petr Kotáb <petrkotab99@gmail.com>
2024-12-03 20:49:11 +0100

    day03

---
+++ day03/src/main.rs
@@ -0,0 +1,21 @@
+use regex::Regex;
+
+fn main() {
+    let result_part1 = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap()
+        .captures_iter(include_str!("../in.txt"))
+        .map(|capture| capture[1].parse::<i32>().unwrap() * capture[2].parse::<i32>().unwrap())
+        .sum::<i32>();
+
+    let mut enabled = true;
+    let mut result_part2 = 0;
+    Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap()
+        .captures_iter(include_str!("../in.txt"))
+        .for_each(|capture| match &capture[0] {
+            "do()" => enabled = true,
+            "don't()" => enabled = false,
+            _ => if enabled { result_part2 += capture[1].parse::<i32>().unwrap() * capture[2].parse::<i32>().unwrap() },
+        });
+
+    println!("part1: {:?}", result_part1);
+    println!("part2: {:?}", result_part2);
+}
