b33fdf739537c1843cb2de01780c6ce5a5f23512
Petr Kotáb <petrkotab99@gmail.com>
2024-12-07 23:55:03 +0100

    Added time measurement to day 03.

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,6 +1,8 @@
+use std::time::Instant;
 use regex::Regex;
 
 fn main() {
+    let now = Instant::now();
     let result_part1 = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap()
         .captures_iter(include_str!("../in.txt"))
         .map(|capture| capture[1].parse::<i32>().unwrap() * capture[2].parse::<i32>().unwrap())
@@ -15,7 +17,9 @@ fn main() {
             "don't()" => enabled = false,
             _ => if enabled { result_part2 += capture[1].parse::<i32>().unwrap() * capture[2].parse::<i32>().unwrap() },
         });
+    let elapsed = now.elapsed();
 
     println!("part1: {:?}", result_part1);
     println!("part2: {:?}", result_part2);
+    println!("elapsed: {:.2?}", elapsed);
 }
